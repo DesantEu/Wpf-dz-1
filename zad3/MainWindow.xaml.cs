4c249348e0f24001fb37a6a50c6a4baee01c3f30
wazzap203203 <wazzap203203@list.ru>
2019-03-10 01:48:31 +0200

    not working calculator

---
+++ zad3/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zad3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string goodstring;
+        string numString;
+
+        int numLength;
+
+        bool lastEq;
+        bool lastSymb;
+        bool first;
+
+        public string Goodstring { get => goodstring; set { goodstring = value; upperText.Text = goodstring; } }
+
+        //actioin 0+ 1- 2* 3/ -1NULL
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            c_click(null,null);
+        }
+
+
+        /// <summary>
+        /// Update Upper Text
+        /// </summary>
+        private void uut()
+        {
+            upperText.Text = Goodstring;
+        }
+
+        private void arrow_click(object sender, RoutedEventArgs e)
+        {
+            numString = numString.Remove(numString.Length);
+        }
+
+        private void action_click(object sender, RoutedEventArgs e)
+        {
+            if (first||lastEq)
+            {
+                first = false;
+                lastEq = false;
+                c_click(null, null);
+                Goodstring += "0";
+            }
+            numLength = 0;
+            if (lastSymb)
+            {
+                Goodstring = goodstring.Remove(goodstring.Length - 3, 3);
+            }
+                Goodstring += $" {(sender as Button).Content.ToString()} ";
+            lastSymb = true;
+        }
+
+        private void equal_click(object sender, RoutedEventArgs e)  
+        {
+            if (Goodstring == "") return;
+            if (lastSymb) Goodstring = Goodstring.Remove(goodstring.Length - 3, 3);
+
+            MessageBox.Show($"parsing {Goodstring}");
+            List<string> nums = new List<string>(Goodstring.Split(' '));
+            double num1;
+            num1 = double.Parse(nums[0]);
+
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                if (nums[i] == "*")
+                {
+                    nums[i - 1] = (double.Parse(nums[i - 1]) * double.Parse(nums[i + 1])).ToString();
+                    nums.RemoveRange(i,2);
+                }else if (nums[i] == "/"){
+                    if(double.Parse(nums[i + 1]) == 0)
+                    {
+                        result.Text = "Делить на ноль нельзя";
+                    }
+                    nums[i - 1] = (double.Parse(nums[i - 1]) / double.Parse(nums[i + 1])).ToString();
+                    nums.RemoveRange(i,2);
+                } 
+                
+            }
+            for (int i = 0; i < nums.Count() -2; i+=2)
+            {
+                if (nums[i + 1] == "-")
+                {
+                    num1 = num1 - double.Parse(nums[i+2]);
+                } else if (nums[i + 1] == "+")
+                {
+                    num1 = num1 + double.Parse(nums[i+2]);
+                }
+            }
+            result.Text = num1.ToString();
+            lastEq = true;
+        }
+
+        private void dot_click(object sender, RoutedEventArgs e)
+        {
+            if (first)
+            {
+                first = false;
+                c_click(null, null);
+                Goodstring += "0";
+            }
+            numLength = 0;
+            Goodstring += ",";
+        }
+
+        private void num_click(object sender, RoutedEventArgs e)
+        {
+            if (first||lastEq)
+            {
+                first = false;
+                lastEq = false;
+                c_click(null, null);
+            }
+            lastSymb = false;
+            Goodstring += (sender as Button).Content.ToString();
+            numLength++;
+        }
+
+        private void c_click(object sender, RoutedEventArgs e)
+        {
+            goodstring = "";
+            numString = "";
+            numLength = 0;
+            lastEq = false;
+            lastSymb = false;
+            first = true;
+        }
+
+        private void CE_click(object sender, RoutedEventArgs e)
+        {
+            if(!lastSymb)
+            goodstring = goodstring.Remove(goodstring.Length + numLength);
+        }
+    }
+}
